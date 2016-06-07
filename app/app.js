4671f7f6488f98f0b2e2f441a1e3dc18f2e5f65e
Wenson Tsai <wensontsai@gmail.com>
2016-06-07 13:27:59 -0400

    styling added

--- app/app.js
+++ app/app.js
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom';
 import Search from './components/search.component';
 import Details from './components/details.component';
 import Player from './components/player.component';
-
+import Progress from './components/progress.component';
+import Footer from './components/footer.component';
 
 class App extends Component {
 	render () {
@@ -13,6 +14,12 @@ class App extends Component {
         <Search />
         <Details title={'Track title'} />
         <Player />
+        <Progress
+          position={'0.3'}
+          elapsed={'00:00'}
+          total={'0:40'}
+        />
+        <Footer />
       </div>
     );
 	}
---
+++ app/components/progress.component.js
@@ -0,0 +1,19 @@
+import React, { Component, PropTypes } from 'react';
+
+class Progress extends Component {
+  render () {
+    return (
+      <div className='progress'>
+        <span className='player__time-elapsed'>{this.props.elapsed}</span>
+        <progress
+          value={this.props.position}
+          max='1'
+        >
+        </progress>
+        <span className="player__time-total">{this.props.total}</span>
+      </div>
+    );
+  }
+}
+
+export default Progress
