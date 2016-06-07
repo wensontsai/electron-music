9f0df45b0520ed844449eeecaafe94c84252e810
Wenson Tsai <wensontsai@gmail.com>
2016-06-07 19:56:41 -0400

    working condition

--- app/app.js
+++ app/app.js
@@ -1,25 +1,18 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 
-import Search from './components/search.component';
-import Details from './components/details.component';
-import Player from './components/player.component';
-import Progress from './components/progress.component';
-import Footer from './components/footer.component';
+// import Search from './components/search.component';
+// import Details from './components/details.component';
+// import Player from './components/player.component';
+// import Progress from './components/progress.component';
+// import Footer from './components/footer.component';
+import AppContainer from './containers/app.container';
 
 class App extends Component {
 	render () {
 		return (
       <div>
-        <Search />
-        <Details title={'Track title'} />
-        <Player />
-        <Progress
-          position={'0.3'}
-          elapsed={'00:00'}
-          total={'0:40'}
-        />
-        <Footer />
+        <AppContainer />
       </div>
     );
 	}
